39ed3045a5c22570d77db7358ff8fb9c8f315422
Thomas <tomgx-github@tomgx.com>
2024-10-25 14:27:54 +0100

    Update AuctionClient.java

--- level1/AuctionClient.java
+++ level1/AuctionClient.java
@@ -1,23 +1,31 @@
-import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 public class AuctionClient {
     public static void main(String[] args) {
         try {
-            Auction auction = (Auction) Naming.lookup("rmi://localhost/Auction");
-            Scanner scanner = new Scanner(System.in);
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            Auction auction = (Auction) registry.lookup("Auction");
 
-            System.out.print("Enter item ID: ");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the item ID to retrieve details: ");
             int itemID = scanner.nextInt();
 
             AuctionItem item = auction.getSpec(itemID);
             if (item != null) {
-                System.out.println(item);
+                System.out.println("\nItem Details:");
+                System.out.println("Item ID: " + item.itemID);
+                System.out.println("Name: " + item.name);
+                System.out.println("Description: " + item.description);
+                System.out.println("Highest Bid: " + item.highestBid);
             } else {
                 System.out.println("Item not found.");
             }
+
             scanner.close();
         } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
     }
