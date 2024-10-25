bbe1cbe3ccfcdb00a79511b975dab8d85f31dbfa
Thomas <tomgx-github@tomgx.com>
2024-10-25 14:30:46 +0100

    Update AuctionItem.java

--- level1/AuctionItem.java
+++ level1/AuctionItem.java
@@ -1,22 +1,8 @@
 import java.io.Serializable;
 
-public class AuctionItem implements java.io.Serializable {
+public class AuctionItem implements Serializable {
     int itemID;
     String name;
     String description;
     int highestBid;
-
-    // Constructor
-    public AuctionItem(int itemID, String name, String description, int highestBid) {
-        this.itemID = itemID;
-        this.name = name;
-        this.description = description;
-        this.highestBid = highestBid;
-    }
-
-    // toString for easy display in the client
-    @Override
-    public String toString() {
-        return "ItemID: " + itemID + ", Name: " + name + ", Description: " + description + ", HighestBid: " + highestBid;
-    }
 }
