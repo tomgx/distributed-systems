623d9eb448b48012cfd732a9855181af30260e00
Thomas <tomgx-github@tomgx.com>
2024-10-25 14:38:06 +0100

    Update AuctionClient.java

--- level1/AuctionClient.java
+++ level1/AuctionClient.java
@@ -1,3 +1,8 @@
+import javax.crypto.Cipher;
+import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
@@ -5,6 +10,14 @@ import java.util.Scanner;
 public class AuctionClient {
     public static void main(String[] args) {
         try {
+            // Load AES key from file
+            SecretKey secretKey;
+            try (FileInputStream fis = new FileInputStream("keys/testKey.aes");
+                 ObjectInputStream ois = new ObjectInputStream(fis)) {
+                secretKey = (SecretKey) ois.readObject();
+            }
+
+            // Connect to RMI server
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             Auction auction = (Auction) registry.lookup("Auction");
 
@@ -12,8 +25,13 @@ public class AuctionClient {
             System.out.print("Enter the item ID to retrieve details: ");
             int itemID = scanner.nextInt();
 
-            AuctionItem item = auction.getSpec(itemID);
-            if (item != null) {
+            // Retrieve and decrypt item details
+            SealedObject sealedItem = auction.getSpec(itemID);
+            if (sealedItem != null) {
+                Cipher cipher = Cipher.getInstance("AES");
+                cipher.init(Cipher.DECRYPT_MODE, secretKey);
+                AuctionItem item = (AuctionItem) sealedItem.getObject(cipher);
+
                 System.out.println("\nItem Details:");
                 System.out.println("Item ID: " + item.itemID);
                 System.out.println("Name: " + item.name);
