1ac18ad09287c483b6df47ee6d9d2bede19d5901
Thomas <tomgx-github@tomgx.com>
2024-10-25 14:36:01 +0100

    Update Auction.java

--- level1/Auction.java
+++ level1/Auction.java
@@ -1,5 +1,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import javax.crypto.SealedObject;
 
 public interface Auction extends Remote {
     public AuctionItem getSpec(int itemID) throws RemoteException;
